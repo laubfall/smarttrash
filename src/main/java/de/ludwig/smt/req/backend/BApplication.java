d3ff3afbdde02cf39f595854a89ac9ec21b37c7d
Daniel Ludwig <d.ludwig@micromata.de>
2016-11-15 14:06:49 +0100

    Menu Creation

--- src/main/java/de/ludwig/smt/req/backend/BApplication.java
+++ src/main/java/de/ludwig/smt/req/backend/BApplication.java
@@ -3,10 +3,10 @@ package de.ludwig.smt.req.backend;
 import de.ludwig.rdd.Requirement;
 import de.ludwig.smt.req.frontend.FOverview;
 import de.ludwig.smt.tec.ElasticSearch;
+import jodd.json.JsonSerializer;
 import jodd.petite.meta.PetiteBean;
 import jodd.petite.meta.PetiteInject;
 import spark.Spark;
-import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 /**
  * Everything to start the whole application.
@@ -16,15 +16,17 @@ import spark.template.thymeleaf.ThymeleafTemplateEngine;
  */
 @Requirement
 @PetiteBean
-public class BApplication {
-	
+public class BApplication
+{
+
 	@PetiteInject
 	protected ElasticSearch es;
-	
+
 	@PetiteInject
 	protected FOverview fOverview;
-	
-	public final void startApplication() {
+
+	public final void startApplication()
+	{
 		startSpark();
 		startElasticsearch();
 	}
@@ -32,11 +34,14 @@ public class BApplication {
 	/**
 	 * Register all Spark Request Handler.
 	 */
-	public final void startSpark() {
-		Spark.get("/", (req, res) -> fOverview.showWelcomePage().apply(req, res), new ThymeleafTemplateEngine());
+	public final void startSpark()
+	{
+		final JsonSerializer jsonSerializer = JsonSerializer.create();
+		Spark.get("/", (req, res) -> fOverview.showWelcomePage().apply(req, res), jsonSerializer::serialize);
 	}
 
-	public final void startElasticsearch() {
+	public final void startElasticsearch()
+	{
 		es.startElasticsearch();
 	}
 }
--- src/main/java/de/ludwig/smt/req/frontend/FOverview.java
+++ src/main/java/de/ludwig/smt/req/frontend/FOverview.java
@@ -1,11 +1,15 @@
 package de.ludwig.smt.req.frontend;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
 
 import de.ludwig.rdd.Requirement;
+import de.ludwig.smt.req.frontend.tec.MenuEntry;
+import de.ludwig.smt.req.frontend.tec.OverviewDataService;
 import jodd.petite.meta.PetiteBean;
+import jodd.petite.meta.PetiteInject;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -19,18 +23,27 @@ import spark.Response;
 @Requirement
 @PetiteBean
 public class FOverview {
+	
+	@PetiteInject
+	private OverviewDataService ods;
+	
 	public BiFunction<Request, Response, ModelAndView> showWelcomePage(){		
 		return (req, res) -> {
-			createFlowOverviewMenu();
+			final Map<String, Object> model = new HashMap<>();
+			createFlowOverviewMenu(model);
 			createFlowOverview();
 			
-			Map<String, Object> model = new HashMap<>();
 			return new ModelAndView(model, "index");
 		};
 	}
 	
-	public void createFlowOverviewMenu(){
-		
+	/**
+	 * Create the menu.
+	 * @param model model object that contains the menu object.
+	 */
+	public void createFlowOverviewMenu(Map<String, Object> model){
+		final List<MenuEntry> createFlowOverviewMenu = ods.createFlowOverviewMenu();
+		model.put("menu", createFlowOverviewMenu);
 	}
 	
 	public Map<String, Object> createFlowOverview(){
