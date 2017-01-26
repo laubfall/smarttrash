aec0bf049a465ed0582291bbf11fabb69f4e2f4a
Daniel Ludwig <d.ludwig@micromata.de>
2017-01-26 17:49:04 +0100

    Show edit / create modal for notes

--- src/main/java/de/ludwig/smt/req/frontend/EditCreateNoteViewService.java
+++ src/main/java/de/ludwig/smt/req/frontend/EditCreateNoteViewService.java
@@ -5,6 +5,8 @@ import java.util.function.BiFunction;
 import de.ludwig.rdd.Requirement;
 import de.ludwig.smt.app.data.Note;
 import de.ludwig.smt.req.frontend.tec.EditCreateNoteModel;
+import de.ludwig.smt.tec.frontend.ModalModelAndView;
+import de.ludwig.smt.tec.frontend.ModalModelObject;
 import de.ludwig.smt.tec.frontend.ModalProvider;
 import jodd.petite.meta.PetiteBean;
 import spark.ModelAndView;
@@ -26,20 +28,24 @@ public class EditCreateNoteViewService implements ModalProvider
 	{
 		return (req, res) -> {
 			final EditCreateNoteModel editCreateNoteModel = new EditCreateNoteModel();
-			Note modelObject = null;
-			if(isNewNote(req)) {
-				modelObject = new Note();
-			} else {
-				
+			Note note = new Note();
+			if (isNewNote(req) == false) {
+				// TODO load the note
 			}
-			return null;
+			
+			editCreateNoteModel.setNote(note);
+
+			final ModalModelObject mmo = new ModalModelObject();
+			mmo.modalContentName("editCreateNote").formActionName("editCreateNote").modelObject(editCreateNoteModel);
+			return new ModalModelAndView(mmo);
 		};
 	}
 
-	public Note loadNoteFillForm(Request req, EditCreateNoteModel modelObject) {
+	public Note loadNoteFillForm(Request req, EditCreateNoteModel modelObject)
+	{
 		return null;
 	}
-	
+
 	public boolean isNewNote(Request req)
 	{
 		return req.queryMap("noteId").hasValue();
